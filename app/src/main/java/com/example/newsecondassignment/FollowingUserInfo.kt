9c63633bf30ca189a713cc24268e985fe7b53a1c
JooYae <jooyae3970@naver.com>
2021-05-17 16:23:02 +0900

    reset main

--- app/src/main/java/com/example/newsecondassignment/FollowingUserInfo.kt
+++
@@ -1,6 +0,0 @@
-package com.example.newsecondassignment
-
-data class FollowingUserInfo (
-    val userImage:String,
-    val userName:String
-)
--- app/src/main/java/com/example/newsecondassignment/RepoInfo.kt
+++
@@ -1,7 +0,0 @@
-package com.example.newsecondassignment
-
-data class RepoInfo (
-    val userRepoName:String,
-    val userRepoDescription:String,
-    val userRepoLanguage:String
-)
