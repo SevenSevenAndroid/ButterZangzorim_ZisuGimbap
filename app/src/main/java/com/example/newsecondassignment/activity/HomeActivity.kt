0b4b3029a08ff2983e4886ac16fdeb60311dc268
cjsjizzu <70841402+cjsjizzu@users.noreply.github.com>
2021-06-13 21:45:01 +0900

    로그아웃

--- app/src/main/java/com/example/newsecondassignment/activity/HomeActivity.kt
+++ app/src/main/java/com/example/newsecondassignment/activity/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.newsecondassignment.RepoInfo
+import com.example.newsecondassignment.SoptUserAuthStorage
+import com.example.newsecondassignment.SoptUserInfo
 import com.example.newsecondassignment.adapter.RepoListAdapter
 import com.example.newsecondassignment.databinding.ActivityHomeBinding
 
@@ -17,6 +19,7 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         moreButtonClickEvent()
+        logoutButtonClickEvent()
 
         val repoListAdapter = RepoListAdapter()
 
@@ -25,48 +28,54 @@ class HomeActivity : AppCompatActivity() {
         repoListAdapter.repoList.addAll(
             listOf<RepoInfo>(
                 RepoInfo(
-                    userRepoName = "이름이 너무 길다이름이 너무 길다이름이 너무 길다이름이 너무 길다이름이 너무 길다이름이 너무 길다이름이 너무 길다",
-                    userRepoDescription = "awesomeawesomeawesomeawesomeawesomeawesomeawesomeawesomeawesomeawesomeawesomeawesomeawesomeawesome",
-                    userRepoLanguage = "kt"
+                    userRepoName = "안드로이드 과제 1",
+                    userRepoDescription = "킹갓제너럴엠페러마제스티골져스프레셔스뷰리풀하이클래스엘레강스럭셔리클래식지니어스원더풀러블리월드탑클래스코드",
+                    userRepoLanguage = "kotlin"
                 ),
                 RepoInfo(
-                    userRepoName = "qqqqq",
-                    userRepoDescription = "awesomeawesomeawesomeawesomeawesomeawesomeawesomeawesomeawesomeawesomeawesomeawesomeawesome",
-                    userRepoLanguage = "kt"
+                    userRepoName = "안드로이드 과제 2",
+                    userRepoDescription = "킹갓제너럴엠페러마제스티골져스프레셔스뷰리풀하이클래스엘레강스럭셔리클래식지니어스원더풀러블리월드탑클래스코드",
+                    userRepoLanguage = "kotlin"
                 ),
                 RepoInfo(
-                    userRepoName = "qqqqq",
-                    userRepoDescription = "awesome",
-                    userRepoLanguage = "kt"
+                    userRepoName = "안드로이드 과제 3",
+                    userRepoDescription = "킹갓제너럴엠페러마제스티골져스프레셔스뷰리풀하이클래스엘레강스럭셔리클래식지니어스원더풀러블리월드탑클래스코드",
+                    userRepoLanguage = "kotlin"
                 ),
                 RepoInfo(
-                    userRepoName = "qqqqq",
-                    userRepoDescription = "awesomeawesomeawesomeawesomeawesomeawesomeawesomeawesomeawesomeawesome",
-                    userRepoLanguage = "kt"
+                    userRepoName = "안드로이드 과제 4",
+                    userRepoDescription = "킹갓제너럴엠페러마제스티골져스프레셔스뷰리풀하이클래스엘레강스럭셔리클래식지니어스원더풀러블리월드탑클래스코드",
+                    userRepoLanguage = "kotlin"
                 ),
                 RepoInfo(
-                    userRepoName = "qqqqq",
-                    userRepoDescription = "awesome",
-                    userRepoLanguage = "kt"
+                    userRepoName = "안드로이드 과제 5",
+                    userRepoDescription = "킹갓제너럴엠페러마제스티골져스프레셔스뷰리풀하이클래스엘레강스럭셔리클래식지니어스원더풀러블리월드탑클래스코드",
+                    userRepoLanguage = "kotlin"
                 ),
                 RepoInfo(
-                    userRepoName = "qqqqq",
-                    userRepoDescription = "awesome",
-                    userRepoLanguage = "kt"
+                    userRepoName = "안드로이드 과제 6",
+                    userRepoDescription = "킹갓제너럴엠페러마제스티골져스프레셔스뷰리풀하이클래스엘레강스럭셔리클래식지니어스원더풀러블리월드탑클래스코드",
+                    userRepoLanguage = "kotlin"
                 ),
                 RepoInfo(
-                    userRepoName = "qqqqq",
-                    userRepoDescription = "awesome",
-                    userRepoLanguage = "kt"
-                ),
-                RepoInfo(
-                    userRepoName = "qqqqq",
-                    userRepoDescription = "awesome",
-                    userRepoLanguage = "kt"
+                    userRepoName = "안드로이드 과제 7",
+                    userRepoDescription = "킹갓제너럴엠페러마제스티골져스프레셔스뷰리풀하이클래스엘레강스럭셔리클래식지니어스원더풀러블리월드탑클래스코드",
+                    userRepoLanguage = "kotlin"
                 )
             )
         )
         repoListAdapter.notifyDataSetChanged()
+    }
+
+    private fun logoutButtonClickEvent() {
+        binding.buttonSignOut.setOnClickListener {
+            with(SoptUserAuthStorage.getInstance(this)) {
+                clearAuthStorage()
+            }
+            val intent = Intent(this@HomeActivity, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
     private fun moreButtonClickEvent() {
